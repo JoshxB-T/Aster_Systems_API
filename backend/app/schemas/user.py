a3469d3f8e2c1b8106af53ccec46c6affbd6c483
JoshxB-T <joshtorrez320@gmail.com>
2026-06-09 18:08:39 -0500

    feat: added user schema for SQLAlchemy

---
+++ backend/app/schemas/user.py
@@ -0,0 +1,21 @@
+from typing import override
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__: str = "user_account"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(255), unique=True)
+    hashed_password: Mapped[str] = mapped_column(String(255))
+    role: Mapped[str] = mapped_column(String(50))
+
+    @override
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, email={self.email!r}, hashed_password={self.hashed_password!r}, role={self.role!r})"
