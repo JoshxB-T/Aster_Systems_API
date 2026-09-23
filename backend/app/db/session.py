40f142564f08f3cf3b15d02e4f136d6fde673b68
JoshxB-T <joshtorrez320@gmail.com>
2026-09-23 01:01:50 -0500

    refactor: moved sqlalchemy table files into models/ from /db

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -3,7 +3,10 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.environ["DATABASE_URL"]
+from dotenv import load_dotenv
+load_dotenv()
+
+DATABASE_URL = os.environ["POSTGRES_HOST_DATABASE_URL"]
 
 engine = create_engine(
     DATABASE_URL,
